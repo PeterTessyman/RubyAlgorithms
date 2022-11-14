aa586d2ae0df4bad37c6a9bb0a4c16fa473e6142
PeterTessyman <petertessyman@btinternet.com>
2022-11-14 14:42:18 +0000

    Update bubblr_sort.rb
    Gutted code to start again.

--- bubblr_sort.rb
+++ bubblr_sort.rb
@@ -1,26 +1,3 @@
 def bubblr_sort arr
-	i = 0
-	return outer_loop arr, i
-end
 
-def inner_loop arr, i, j
-	if j < i
-		j =+ 1
-		if arr[j] < arr[i]
-			arr[i], arr[j] = arr[j], arr[i]
-		end
-		return inner_loop arr, i, j
-	else
-		return arr, i
-	end
-end
-
-def outer_loop arr, i
-	if i < arr.length
-		j = i
-		i =+ 1
-		return inner_loop arr, i, j
-	else
-		return outer_loop arr, i
-	end
 end
